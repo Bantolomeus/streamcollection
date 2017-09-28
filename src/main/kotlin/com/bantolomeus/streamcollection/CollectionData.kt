44a127aba56d9e9c55b64dad798e67b03df22009
chrb <chrb@spreadshirt.net>
2017-09-28 10:11:43 +0200

    #1 mutable map<Int to String> created

--- src/main/kotlin/com/bantolomeus/streamcollection/CollectionData.kt
+++ src/main/kotlin/com/bantolomeus/streamcollection/CollectionData.kt
@@ -6,4 +6,9 @@ class CollectionData {
         return mutableMapOf("1" to "Hello", "2" to "world", "3" to "!", "4" to "Are",
                             "5" to "you", "6" to "ready", "7" to "?")
     }
+
+    fun fillOutMapOfIntToString(): MutableMap<Int, String> {
+        return mutableMapOf(1 to "Hello", 2 to "world", 3 to "!", 4 to "Are",
+                5 to "you", 6 to "ready", 7 to "?")
+    }
 }
