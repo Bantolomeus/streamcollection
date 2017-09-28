d80e07f1569d2e577ff606af75fd878b33448127
chrb <chrb@spreadshirt.net>
2017-09-28 08:49:01 +0200

    initial commit

---
+++ src/main/kotlin/com/bantolomeus/streamcollection/StreamcollectionApplication.kt
@@ -0,0 +1,11 @@
+package com.bantolomeus.streamcollection
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class StreamcollectionApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(StreamcollectionApplication::class.java, *args)
+}
